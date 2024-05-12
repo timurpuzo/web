02a3c69c202ef164b4e65e47e8b6be8915d79190
timurpuzo <timur.puzo@stu.ibu.edu.ba>
2024-05-12 22:56:42 +0200

    Project 2

--- Project 2/js/scripts.js
+++ Project 2/js/scripts.js
@@ -8,3 +8,25 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+let images = document.querySelectorAll('img')
+let wrapper = document.getElementById('wrapper')
+let imgWrapper = document.getElementById('fullImg')
+let close = document.querySelector('span')
+console.log(images);
+console.log(wrapper);
+console.log(imgWrapper);
+console.log(close);
+
+images.forEach((img, index) => {
+    img.addEventListener('click',()=>{
+        openModal(assets/img$(index).jpg)
+    })
+})
+
+
+function openModal(pic){
+    wrapper.style.display = "flex";
+    imgWrapper.src = pic;
+}
+
